bf529801f28cf942f49d9282b5338c157bf796e7
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-03-31 15:30:37 -0400

    tuning

--- generate_toy_data.py
+++ generate_toy_data.py
@@ -1,19 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from toymodel import mass2radius
 
 def generate_toy_data(nobs, nsim):
 
     # observed data, or "truth"
-    msini_obs = np.random.randn(nobs)*.5
-    np.savetxt("observed.txt", np.transpose((msini_obs)))
+    m_obs = np.random.randn(nobs)*.1 + 2
+    i = np.random.uniform(0, np.pi, nobs)
+    np.savetxt("observed.txt", np.transpose((m_obs*np.sin(i))))
 
     # data for simulations (in reality, taken from Kepler)
-    radii = .5*np.random.randn(nsim)  # 1000 planets with mean = 1 earth radius
+    m_for_sim = np.random.randn(nsim)*.1 + 2
+    truth = [.9, .2]
+    radii = mass2radius(truth, m_for_sim)
     periods = np.random.uniform(1., 100., nsim)  # days
-    star_masses = .5*np.random.randn(nsim) + .3  # M_sun
-    inclinations = np.random.uniform(0, 360, nsim)
+    star_masses = .01*np.random.randn(nsim) + .5  # M_sun
+    inclinations = np.random.uniform(0, np.pi, nsim)
     data = np.vstack((radii, periods, star_masses, inclinations))
     np.savetxt("sim.txt", data.T)
 
 if __name__ == "__main__":
-    generate_toy_data(100, 1000)
+    generate_toy_data(1000, 1000)
--- toymodel.py
+++ toymodel.py
@@ -13,9 +13,14 @@ def radius2mass(theta, radius):
     m, c = theta
     return m * radius + c
 
+# convert the radii of the imaginary planets into masses
+def mass2radius(theta, mass):
+    m, c = theta
+    return (mass - c) / m
+
 def rv_K(P, m_star, m_planet, inc):
-    m_star *= const.M_sun
-    m_planet *= const.M_earth
+    m_star_kg = m_star * 1.9891e30
+    m_planet_kg = m_planet * 5.972e24
     """
     Calculate RV semi-amplitude of body 1 for orbital period P and mass
     function f, = m1 (sin(incl))**2 / (m1+m2)**3, where m1 and m2 are
@@ -24,21 +29,17 @@ def rv_K(P, m_star, m_planet, inc):
     edge-on). Takes period in days, m_star in solar masses, m_planet in
     Earth masses.
     """
-    f = m_star * np.sin(inc) ** 2 / (m_star + m_planet) ** 3
-    return (2 * np.pi * 6.67e-11 / (P*86400)**2)**(1/3.) * f
-
-# Given the K of each planet, calculate whether it would be
-# detected by an RV survey (simplified for now)
-def detected(K, P):
-    l = (K > 10) * (P < 200)
-    return l
+    f = m_planet_kg * np.sin(inc) / (m_star_kg + m_planet)**(2/3.)
+    return (2 * np.pi * 6.67e-11 / (P*86400))**(1/3.) * f
 
 # create a population of the synthesised planets that would be detected
 def populationize(theta, sim_data):
     r_sim, p_sim, m_star, inc = sim_data
     m_planet = radius2mass(theta, r_sim)  # calculate mass
     K = rv_K(p_sim, m_star, m_planet, inc)  # calculate semi-amplitude
-    l = detected(K, p_sim)  # throw away undetectable planets
+
+    # detected?
+    l = (K > .1) * (p_sim < 100)
     return m_planet[l] * np.sin(inc[l])  # return msinis
 
 # compute the sufficient statistic, the KS statistic of the mass distributions
@@ -48,6 +49,8 @@ def KS_statistic(m_obs, m_sim):
 # decide whether to accept or reject
 def accept_reject(theta, msini_obs, sim_data, tolerance):
     msini_sim = populationize(theta, sim_data)  # simulated msinis
+
+    # accept/reject
     if KS_statistic(msini_obs, msini_sim) < tolerance:
         return theta
     else:
@@ -59,8 +62,8 @@ def ABC_sampling(msini_obs, sim_data, tolerance, nsamp):
 
     # generate parameter samples
     thetas = np.zeros((2, nsamp))
-    thetas[0, :] = 2*np.random.randn(nsamp)*.1  # gradient
-    thetas[1, :] = .5*np.random.randn(nsamp)*.1  # intercept
+    thetas[0, :] = np.random.randn(nsamp)*.01 + 1  # slope
+    thetas[1, :] = np.random.randn(nsamp)*.5  # intercept
     keepsies = np.zeros_like(thetas)
 
     # determine whether samples are accepted or rejected
@@ -71,22 +74,25 @@ def ABC_sampling(msini_obs, sim_data, tolerance, nsamp):
     parameters = np.zeros((2, len(keepsies[0, :][l])))
     parameters[0] = keepsies[0, :][l]
     parameters[1] = keepsies[1, :][l]
-    return parameters
 
-# approximate the posterior from the pooled parameters
-def ABC_posterior(parameters):
     plt.clf()
     plt.subplot(2, 1, 1)
     plt.hist(parameters[0])
     plt.subplot(2, 1, 2)
     plt.hist(parameters[1])
-    plt.savefig("ABC_posterior")
+    plt.show()
+    return parameters
+
+# approximate the posterior from the pooled parameters
+# def ABC_posterior(parameters):
 
 if __name__ == "__main__":
+
+    # load data
     msini_obs, r_sim, p_sim, m_star, inc = load_data()
-    plt.clf()
-    plt.hist(msini_obs)
-    plt.show()
-    assert 0
     sim_data = np.vstack((r_sim, p_sim, m_star, inc))
-    ABC_sampling(msini_obs, sim_data, tolerance, nsamp)
+
+    # do some ABC
+    tolerance, nsamp = .2, 1000
+    parameters = ABC_sampling(msini_obs, sim_data, tolerance, nsamp)
+    ABC_posterior(parameters)
